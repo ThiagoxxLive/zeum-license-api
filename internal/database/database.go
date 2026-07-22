8940394ff9c0d32a7ef675d3d88f2f5ebbaa9838
Thiago Marcondes <thiagoxx@live.com>
2026-07-22 19:21:34 -0300

    feat: add connection pool

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"time"
+
 	"zeum-license-api/internal/config"
 
 	"gorm.io/driver/mysql"
@@ -8,5 +10,23 @@ import (
 )
 
 func NewConnection(cfg *config.Config) (*gorm.DB, error) {
-	return gorm.Open(mysql.Open(cfg.DSN()), &gorm.Config{})
+
+	db, err := gorm.Open(mysql.Open(cfg.DSN()), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(20)
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	sqlDB.SetConnMaxIdleTime(1 * time.Minute)
+
+	return db, nil
 }
